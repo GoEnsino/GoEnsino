c6e28d3ca0929e0dab10e73950d9123ab5e58088
Isac Moura <contatoisacmoura@outlook.com>
2021-05-30 12:19:38 -0300

    Implementing student profile information

--- src/paginas/Conteudo.jsx
+++ src/paginas/Conteudo.jsx
@@ -3,8 +3,14 @@ const Home = () => {
 
   return (
     <main>
+      <div className="imgcontainer">
+        <img src="https://static.imasters.com.br/wp-content/uploads/2019/08/20134733/isacmoura.jpg.jpg" alt="Avatar" className="imgprofile"></img>
+        <div className="student">
+          <h1 className="name">Isac Moura</h1>
+          <h2 className="school">EEEP Maria Dolores Alcântara e Silva</h2>
+        </div>
+      </div>
       <div className="container">
-
         <div className="bloco">
             
         </div>
